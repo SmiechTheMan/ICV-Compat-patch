e9e6a3fb08c99a03a6b7fab54a8c9ad1837a9bf1
Igneo <Igneo220@gmail.com>
2024-08-01 22:26:03 -0400

    multiplayer fix

--- src/main/java/net/igneo/icv/event/ModEvents.java
+++ src/main/java/net/igneo/icv/event/ModEvents.java
@@ -161,125 +161,130 @@ public class ModEvents {
                 } else {
                     armor = "armor:null";
                 }
-                if (event.player.getInventory().getArmor(j).serializeNBT().toString().contains("Trims")) {
+                if (event.player.getInventory().getArmor(j).serializeNBT().toString().contains("Trim")) {
                     //System.out.println(event.player.getInventory().getArmor(j).getTag().get("Enchantments"));
-                    armor = armor + "," + "trim:" + event.player.getInventory().getArmor(j).getTag().get("Enchantments");
+                    //System.out.println("omg!!! a trim!!!");
+                    armor = armor + "," + "trim:" + event.player.getInventory().getArmor(j).getTag().get("Trim");
                 } else {
+                    //System.out.println("lebruh");
                     armor = armor + "," + "trim:null";
                 }
                 //System.out.println(armor);
                 if (!armor.equals(enchVar.getPlayerArmor().get(j))) {
-                    //System.out.println("we are NOT the same");
+                    System.out.println("we are NOT the same");
                     enchVar.setPlayerArmor(armor,j);
-                    applyBuffs(event.player);
                     if (event.player.level().isClientSide) {
-                        uniPlayer = Minecraft.getInstance().player;
-                        if (!event.player.getInventory().armor.get(j).toString().contains("air")) {
-                            List<Enchantment> enchantlist = new ArrayList<Enchantment>(event.player.getInventory().armor.get(j).getAllEnchantments().keySet());
-                            if (!enchantlist.isEmpty()) {
-                                String enchID = enchantlist.get(0).getFullname(1).toString();
-                                enchID = enchID.replace("', args=[]}[style={color=gray}]", "");
-                                enchID = enchID.replace("translation{key='enchantment.icv.", "");
+                        if (event.player == Minecraft.getInstance().player) {
+                            uniPlayer = Minecraft.getInstance().player;
+                            if (!event.player.getInventory().armor.get(j).toString().contains("air")) {
+                                List<Enchantment> enchantlist = new ArrayList<Enchantment>(event.player.getInventory().armor.get(j).getAllEnchantments().keySet());
+                                if (!enchantlist.isEmpty()) {
+                                    String enchID = enchantlist.get(0).getFullname(1).toString();
+                                    enchID = enchID.replace("', args=[]}[style={color=gray}]", "");
+                                    enchID = enchID.replace("translation{key='enchantment.icv.", "");
+                                    switch (j) {
+                                        case 0:
+                                            switch (enchID) {
+                                                case "comet_strike":
+                                                    //System.out.println(enchID);
+                                                    enchVar.setBootID(1);
+                                                    break;
+                                                case "double_jump":
+                                                    enchVar.setBootID(2);
+                                                    break;
+                                                case "momentum":
+                                                    enchVar.setBootID(3);
+                                                    break;
+                                                case "sky_charge":
+                                                    enchVar.setBootID(4);
+                                                    break;
+                                                case "stone_caller":
+                                                    enchVar.setBootID(5);
+                                                    break;
+                                            }
+                                            break;
+                                        case 1:
+                                            refreshLegs = true;
+                                            switch (enchID) {
+                                                case "acrobatic":
+                                                    enchVar.setLegID(1);
+                                                    break;
+                                                case "crush":
+                                                    enchVar.setLegID(2);
+                                                    break;
+                                                case "incapacitate":
+                                                    enchVar.setLegID(3);
+                                                    break;
+                                                case "judgement":
+                                                    enchVar.setLegID(4);
+                                                    break;
+                                                case "train_dash":
+                                                    enchVar.setLegID(5);
+                                                    break;
+                                            }
+                                            break;
+                                        case 2:
+                                            refreshChest = true;
+                                            switch (enchID) {
+                                                case "concussion":
+                                                    enchVar.setChestID(1);
+                                                    break;
+                                                case "flare":
+                                                    enchVar.setChestID(2);
+                                                    break;
+                                                case "parry":
+                                                    enchVar.setChestID(3);
+                                                    break;
+                                                case "siphon":
+                                                    enchVar.setChestID(4);
+                                                    break;
+                                                case "wardenspine":
+                                                    enchVar.setChestID(5);
+                                                    break;
+                                            }
+                                            break;
+                                        case 3:
+                                            refreshHelmet = true;
+                                            switch (enchID) {
+                                                case "black_hole":
+                                                    enchVar.setHelmetID(1);
+                                                    break;
+                                                case "blizzard":
+                                                    enchVar.setHelmetID(2);
+                                                    break;
+                                                case "flamethrower":
+                                                    enchVar.setHelmetID(3);
+                                                    break;
+                                                case "smite":
+                                                    enchVar.setHelmetID(4);
+                                                    break;
+                                                case "warden_scream":
+                                                    enchVar.setHelmetID(5);
+                                                    break;
+                                            }
+                                            break;
+                                    }
+                                }
+                            } else {
                                 switch (j) {
                                     case 0:
-                                        switch (enchID) {
-                                            case "comet_strike":
-                                                //System.out.println(enchID);
-                                                enchVar.setBootID(1);
-                                                break;
-                                            case "double_jump":
-                                                enchVar.setBootID(2);
-                                                break;
-                                            case "momentum":
-                                                enchVar.setBootID(3);
-                                                break;
-                                            case "sky_charge":
-                                                enchVar.setBootID(4);
-                                                break;
-                                            case "stone_caller":
-                                                enchVar.setBootID(5);
-                                                break;
-                                        }
+                                        System.out.println("ohhh");
+                                        enchVar.setBootID(0);
                                         break;
                                     case 1:
-                                        refreshLegs = true;
-                                        switch (enchID) {
-                                            case "acrobatic":
-                                                enchVar.setLegID(1);
-                                                break;
-                                            case "crush":
-                                                enchVar.setLegID(2);
-                                                break;
-                                            case "incapacitate":
-                                                enchVar.setLegID(3);
-                                                break;
-                                            case "judgement":
-                                                enchVar.setLegID(4);
-                                                break;
-                                            case "train_dash":
-                                                enchVar.setLegID(5);
-                                                break;
-                                        }
+                                        enchVar.setLegID(0);
                                         break;
                                     case 2:
-                                        refreshChest = true;
-                                        switch (enchID) {
-                                            case "concussion":
-                                                enchVar.setChestID(1);
-                                                break;
-                                            case "flare":
-                                                enchVar.setChestID(2);
-                                                break;
-                                            case "parry":
-                                                enchVar.setChestID(3);
-                                                break;
-                                            case "siphon":
-                                                enchVar.setChestID(4);
-                                                break;
-                                            case "wardenspine":
-                                                enchVar.setChestID(5);
-                                                break;
-                                        }
+                                        enchVar.setChestID(0);
                                         break;
                                     case 3:
-                                        refreshHelmet = true;
-                                        switch (enchID) {
-                                            case "black_hole":
-                                                enchVar.setHelmetID(1);
-                                                break;
-                                            case "blizzard":
-                                                enchVar.setHelmetID(2);
-                                                break;
-                                            case "flamethrower":
-                                                enchVar.setHelmetID(3);
-                                                break;
-                                            case "smite":
-                                                enchVar.setHelmetID(4);
-                                                break;
-                                            case "warden_scream":
-                                                enchVar.setHelmetID(5);
-                                                break;
-                                        }
+                                        enchVar.setHelmetID(0);
                                         break;
                                 }
                             }
-                        } else {
-                            switch (j) {
-                                case 0:
-                                    System.out.println("ohhh");
-                                    enchVar.setBootID(0);
-                                    break;
-                                case 1:
-                                    enchVar.setLegID(0);
-                                    break;
-                                case 2:
-                                    enchVar.setChestID(0);
-                                    break;
-                                case 3:
-                                    enchVar.setHelmetID(0);
-                                    break;
-                            }
                         }
+                    } else {
+                        applyBuffs(event.player);
                     }
                 }
             }
@@ -361,7 +366,7 @@ public class ModEvents {
                 }
             }
 
-            if (FMLEnvironment.dist.isClient()) {
+            if (FMLEnvironment.dist.isClient() && event.player == Minecraft.getInstance().player) {
                 //System.out.println(enchVar.getBootID());
                 //if (enchVar.getBootID() > 0) {
                     switch (enchVar.getBootID()) {
@@ -567,8 +572,8 @@ public class ModEvents {
                 }
             }
             if (shaperTrim > 0 || shaperTrim != enchVar.getShaperBuff()) {
-                player.getAttributes().getInstance(Attributes.MOVEMENT_SPEED).removeModifier(SHAPER_TOUGH_MODIFIER_UUID);
-                player.getAttributes().getInstance(Attributes.MOVEMENT_SPEED).addTransientModifier(new AttributeModifier(SHAPER_TOUGH_MODIFIER_UUID, "Shaper toughness boost", (double) shaperTrim * 2, AttributeModifier.Operation.ADDITION));
+                player.getAttributes().getInstance(Attributes.ARMOR_TOUGHNESS).removeModifier(SHAPER_TOUGH_MODIFIER_UUID);
+                player.getAttributes().getInstance(Attributes.ARMOR_TOUGHNESS).addTransientModifier(new AttributeModifier(SHAPER_TOUGH_MODIFIER_UUID, "Shaper toughness boost", (double) shaperTrim * 2, AttributeModifier.Operation.ADDITION));
                 enchVar.setShaperBuff(shaperTrim);
             }
             if (wayTrim > 0 || wayTrim != enchVar.getWayBuff()) {
