e3a0e59c055d879bfa5d67d777feaeb3b05938c0
Smiech <masternike1207@gmail.com>
2025-07-16 09:25:15 +0200

    Took out non-mixin methods

--- src/main/java/net/igneo/icv/mixin/EnchantmentMenuMixin.java
+++ src/main/java/net/igneo/icv/mixin/EnchantmentMenuMixin.java
@@ -1,6 +1,7 @@
 package net.igneo.icv.mixin;
 
 import net.igneo.icv.enchantment.ModEnchantments;
+import net.igneo.icv.enchantment.enchantsIntoMixin.EnchantsForMixinInject;
 import net.igneo.icv.event.ModEvents;
 import net.igneo.icv.networking.ModMessages;
 import net.igneo.icv.networking.packet.EnchTableUpdateS2CPacket;
@@ -41,26 +42,10 @@ import org.spongepowered.asm.mixin.Unique;
 import java.util.ArrayList;
 import java.util.List;
 
+import static net.igneo.icv.enchantment.enchantsIntoMixin.EnchantsForMixinInject.*;
+
 @Mixin(value = EnchantmentMenu.class,priority = 999999999)
 public class EnchantmentMenuMixin extends AbstractContainerMenu {
-    @Unique
-    private static List<Enchantment> HELM_ENCHANTS = new ArrayList<Enchantment>();
-    @Unique
-    private static List<Enchantment> CHEST_ENCHANTS = new ArrayList<Enchantment>();
-    @Unique
-    private static List<Enchantment> LEG_ENCHANTS = new ArrayList<Enchantment>();
-    @Unique
-    private static List<Enchantment> BOOT_ENCHANTS = new ArrayList<Enchantment>();
-    @Unique
-    private static List<Enchantment> WEAPON_ENCHANTS = new ArrayList<Enchantment>();
-    @Unique
-    private static List<Enchantment> BOW_ENCHANTS = new ArrayList<Enchantment>();
-    @Unique
-    private static List<Enchantment> CROSSBOW_ENCHANTS = new ArrayList<Enchantment>();
-    @Unique
-    private static List<Enchantment> TRIDENT_ENCHANTS = new ArrayList<Enchantment>();
-    @Unique
-    private static List<Enchantment> TOOL_ENCHANTS = new ArrayList<Enchantment>();
     @Shadow
     private final RandomSource random = RandomSource.create();
     @Shadow
@@ -70,8 +55,8 @@ public class EnchantmentMenuMixin extends AbstractContainerMenu {
     @Shadow
     public final int[] levelClue = new int[]{-1, -1, -1};
 
-    private int localEnchShift = 0;
-    private int localLength = 0;
+    private int localEnchShift = returnLEnchShift();
+    private int localLength = returnLLength();
     private ServerPlayer player = null;
     @Shadow
     private final Container enchantSlots = new SimpleContainer(2) {
@@ -122,7 +107,7 @@ public class EnchantmentMenuMixin extends AbstractContainerMenu {
             } else {
                 this.access.execute((level, tablePos) -> {
                     ItemStack itemstack2 = itemstack;
-                    List<EnchantmentInstance> list = this.getChiselEnchantmentList(level,tablePos,itemstack);
+                    List<EnchantmentInstance> list = getChiselEnchantmentList(level,tablePos,itemstack);
                     if (!list.isEmpty()) {
                         pPlayer.onEnchantmentPerformed(itemstack, 1);
                         boolean flag = itemstack.is(Items.BOOK);
@@ -170,13 +155,13 @@ public class EnchantmentMenuMixin extends AbstractContainerMenu {
             if (this.localLength > 3) {
                 if (pId == -1) {
                     if (this.localEnchShift + 3 < this.localLength) {
-                        ++this.localEnchShift;
+                        setLEnchShift(++localEnchShift);
                     }
                 } else if (pId == -2 && this.localEnchShift > 0) {
-                    --this.localEnchShift;
+                    setLEnchShift(--localEnchShift);
                 }
             } else {
-                this.localEnchShift = 0;
+                setLEnchShift(0);;
             }
             if (pPlayer instanceof ServerPlayer) {
                 ModMessages.sendToPlayer(new EnchTableUpdateS2CPacket(this.localEnchShift), (ServerPlayer) pPlayer);
@@ -195,18 +180,18 @@ public class EnchantmentMenuMixin extends AbstractContainerMenu {
         if (this.player != null) {
             ModMessages.sendToPlayer(new EnchTableUpdateS2CPacket(this.localEnchShift), (ServerPlayer) player);
         }
-        updateEnchantmentLists();
+        EnchantsForMixinInject.updateEnchantmentListsDoer();
         if (pInventory == this.enchantSlots) {
             ItemStack itemstack = pInventory.getItem(0);
-            if (!itemstack.isEmpty() && itemstack.isEnchantable() && checkValid(itemstack)) {
+            if (!itemstack.isEmpty() && itemstack.isEnchantable() && checkValidICV(itemstack)) {
                 this.access.execute((level, tablePos) -> {
                     float j = 0;
                     for(int k = 0; k < 3; ++k) {
                         this.costs[k] = 1;
-                      }
+                    }
 
                     for(int l = 0; l < 3; ++l) {
-                        List<EnchantmentInstance> list = this.getChiselEnchantmentList(level,tablePos,itemstack);
+                        List<EnchantmentInstance> list = getChiselEnchantmentList(level,tablePos,itemstack);
                         if (list != null && !list.isEmpty()) {
                             if(l < list.size()) {
                                 EnchantmentInstance enchantmentinstance = list.get(l + this.localEnchShift);
@@ -221,8 +206,8 @@ public class EnchantmentMenuMixin extends AbstractContainerMenu {
                                 this.enchantClue[i] = -1;
                                 this.levelClue[i] = -1;
                             }
-                            this.localEnchShift = 0;
-                            this.localLength = 0;
+                            setLEnchShift(0);
+                            setLLength(0);
                         }
                     }
 
@@ -234,49 +219,12 @@ public class EnchantmentMenuMixin extends AbstractContainerMenu {
                     this.enchantClue[i] = -1;
                     this.levelClue[i] = -1;
                 }
-                this.localEnchShift = 0;
-                this.localLength = 0;
+                setLEnchShift(0);
+                setLLength(0);
             }
         }
 
     }
-
-    private boolean checkValid(ItemStack pStack) {
-        boolean valid = false;
-        if (LivingEntity.getEquipmentSlotForItem(pStack).equals(EquipmentSlot.MAINHAND)) {
-            if (pStack.getItem().equals(Items.BOW)) {
-                valid = true;
-            } else if (pStack.getItem().equals(Items.CROSSBOW)) {
-                valid = true;
-            } else if (pStack.getItem().equals(Items.TRIDENT)) {
-                valid = true;
-            } else if (pStack.getItem().toString().contains("sword") ||
-                    pStack.getItem().toString().contains("scythe") ||
-                    pStack.getItem().toString().contains("glaive") ||
-                    pStack.getItem().toString().contains("halberd") ||
-                    pStack.getItem().toString().contains("hammer") ||
-                    pStack.getItem().toString().contains("rapier") ||
-                    pStack.getItem().toString().contains("spear") ||
-                    pStack.getItem().toString().contains("katana") ||
-                    pStack.getItem().toString().contains("mace")) {
-                valid = true;
-            } else if (pStack.getItem().toString().contains("axe") ||
-                    pStack.getItem().toString().contains("hoe") ||
-                    pStack.getItem().toString().contains("shovel")) {
-                valid = true;
-            }
-        } else if (LivingEntity.getEquipmentSlotForItem(pStack).equals(EquipmentSlot.HEAD)) {
-            valid = true;
-        } else if (LivingEntity.getEquipmentSlotForItem(pStack).equals(EquipmentSlot.CHEST)) {
-            valid = true;
-        } else if (LivingEntity.getEquipmentSlotForItem(pStack).equals(EquipmentSlot.LEGS)) {
-            valid = true;
-        } else if (LivingEntity.getEquipmentSlotForItem(pStack).equals(EquipmentSlot.FEET)) {
-            valid = true;
-        }
-        return valid;
-    }
-
     /**
      * @author Igneo220
      * @reason rewriting enchantment table
@@ -285,200 +233,17 @@ public class EnchantmentMenuMixin extends AbstractContainerMenu {
     public void removed(Player pPlayer) {
         super.removed(pPlayer);
         ModEvents.usedEnchTable = null;
-        this.localEnchShift = 0;
+        setLEnchShift(0);
         if (pPlayer instanceof ServerPlayer) {
             ModMessages.sendToPlayer(new EnchTableUpdateS2CPacket(this.localEnchShift), (ServerPlayer) pPlayer);
         }
-        this.localLength = 0;
+        setLLength(0);
         ModEvents.enchLength = 0;
         this.access.execute((p_39469_, p_39470_) -> {
             this.clearContainer(pPlayer, this.enchantSlots);
         });
     }
 
-    @Unique
-    public List<EnchantmentInstance> getChiselEnchantmentList(Level level, BlockPos tablePos, ItemStack enchItem) {
-        //this.random.setSeed((long) (this.enchantmentSeed.get() + pEnchantSlot));
-        List<EnchantmentInstance> list = new ArrayList<EnchantmentInstance>();
-        for(BlockPos blockpos : EnchantmentTableBlock.BOOKSHELF_OFFSETS) {
-            if (level.getBlockState(tablePos.offset(blockpos)).getBlock().equals(Blocks.CHISELED_BOOKSHELF)) {
-                ChiseledBookShelfBlockEntity bookShelf = (ChiseledBookShelfBlockEntity) level.getBlockEntity(tablePos.offset(blockpos));
-                int i = 5;
-                while (i > -1) {
-                    if (!bookShelf.getItem(i).isEmpty()) {
-                        for (Enchantment enchantment : EnchantmentHelper.getEnchantments(bookShelf.getItem(i)).keySet()) {
-                            if (checkValidEquipmentSlot(enchItem,enchantment)) {
-                                boolean add = true;
-                                for (EnchantmentInstance enchantment1 : list) {
-                                    if (enchantment1.enchantment.equals(enchantment)) {
-                                        add = false;
-                                        break;
-                                    }
-                                }
-                                if (add) {
-                                    list.add(new EnchantmentInstance(enchantment, 1));
-                                }
-                            }
-                        }
-                    }
-                    --i;
-                }
-            }
-        }
-        if (list.size() != this.localLength) {
-            this.localEnchShift = 0;
-        }
-        this.localLength = list.size();
-        return list;
-    }
-    private boolean checkValidEquipmentSlot(ItemStack pStack, Enchantment enchantment) {
-        if (pStack.isEnchantable()  && checkValid(pStack)) {
-            if (LivingEntity.getEquipmentSlotForItem(pStack).equals(EquipmentSlot.MAINHAND)) {
-                if (pStack.getItem().equals(Items.BOW)) {
-                    boolean add = false;
-                    for (Enchantment checkEnchant : BOW_ENCHANTS) {
-                        if (enchantment == checkEnchant) {
-                            add = true;
-                        }
-                    }
-                    return add;
-                } else if (pStack.getItem().equals(Items.CROSSBOW)) {
-                    boolean add = false;
-                    for (Enchantment checkEnchant : CROSSBOW_ENCHANTS) {
-                        if (enchantment == checkEnchant) {
-                            add = true;
-                        }
-                    }
-                    return add;
-                } else if (pStack.getItem().equals(Items.TRIDENT)) {
-                    boolean add = false;
-                    for (Enchantment checkEnchant : TRIDENT_ENCHANTS) {
-                        if (enchantment == checkEnchant) {
-                            add = true;
-                        }
-                    }
-                    return add;
-                } else if (pStack.getItem().toString().contains("sword") ||
-                        pStack.getItem().toString().contains("scythe") ||
-                        pStack.getItem().toString().contains("glaive") ||
-                        pStack.getItem().toString().contains("halberd") ||
-                        pStack.getItem().toString().contains("hammer") ||
-                        pStack.getItem().toString().contains("rapier") ||
-                        pStack.getItem().toString().contains("spear") ||
-                        pStack.getItem().toString().contains("katana") ||
-                        pStack.getItem().toString().contains("mace")) {
-                    boolean add = false;
-                    for (Enchantment checkEnchant : WEAPON_ENCHANTS) {
-                        if (enchantment == checkEnchant) {
-                            add = true;
-                        }
-                    }
-                    return add;
-                } else if (pStack.getItem().toString().contains("axe") ||
-                        pStack.getItem().toString().contains("hoe") ||
-                        pStack.getItem().toString().contains("shovel")) {
-                    boolean add = false;
-                    for (Enchantment checkEnchant : TOOL_ENCHANTS) {
-                        if (enchantment == checkEnchant) {
-                            add = true;
-                        }
-                    }
-                    return add;
-                }
-            } else if (LivingEntity.getEquipmentSlotForItem(pStack).equals(EquipmentSlot.HEAD)) {
-                boolean add = false;
-                for (Enchantment checkEnchant : HELM_ENCHANTS) {
-                    if (enchantment == checkEnchant) {
-                        add = true;
-                    }
-                }
-                return add;
-            } else if (LivingEntity.getEquipmentSlotForItem(pStack).equals(EquipmentSlot.CHEST)) {
-                boolean add = false;
-                for (Enchantment checkEnchant : CHEST_ENCHANTS) {
-                    if (enchantment == checkEnchant) {
-                        add = true;
-                    }
-                }
-                return add;
-            } else if (LivingEntity.getEquipmentSlotForItem(pStack).equals(EquipmentSlot.LEGS)) {
-                boolean add = false;
-                for (Enchantment checkEnchant : LEG_ENCHANTS) {
-                    if (enchantment == checkEnchant) {
-                        add = true;
-                    }
-                }
-                return add;
-            } else if (LivingEntity.getEquipmentSlotForItem(pStack).equals(EquipmentSlot.FEET)) {
-                boolean add = false;
-                for (Enchantment checkEnchant : BOOT_ENCHANTS) {
-                    if (enchantment == checkEnchant) {
-                        add = true;
-                    }
-                }
-                return add;
-            }
-        }
-        return false;
-    }
-    @Unique
-    private void updateEnchantmentLists() {
-        if (HELM_ENCHANTS.isEmpty()) {
-            HELM_ENCHANTS.add(ModEnchantments.BLACK_HOLE.get());
-            HELM_ENCHANTS.add(ModEnchantments.BLIZZARD.get());
-            HELM_ENCHANTS.add(ModEnchantments.FLAMETHROWER.get());
-            HELM_ENCHANTS.add(ModEnchantments.SMITE.get());
-            HELM_ENCHANTS.add(ModEnchantments.WARDEN_SCREAM.get());
-        }
-        if (CHEST_ENCHANTS.isEmpty()) {
-            CHEST_ENCHANTS.add(ModEnchantments.CONCUSSION.get());
-            CHEST_ENCHANTS.add(ModEnchantments.FLARE.get());
-            CHEST_ENCHANTS.add(ModEnchantments.PARRY.get());
-            CHEST_ENCHANTS.add(ModEnchantments.SIPHON.get());
-            CHEST_ENCHANTS.add(ModEnchantments.WARDENSPINE.get());
-        }
-        if (LEG_ENCHANTS.isEmpty()) {
-            LEG_ENCHANTS.add(ModEnchantments.ACROBATIC.get());
-            LEG_ENCHANTS.add(ModEnchantments.CRUSH.get());
-            LEG_ENCHANTS.add(ModEnchantments.INCAPACITATE.get());
-            LEG_ENCHANTS.add(ModEnchantments.JUDGEMENT.get());
-            LEG_ENCHANTS.add(ModEnchantments.TRAIN_DASH.get());
-        }
-        if (BOOT_ENCHANTS.isEmpty()) {
-            BOOT_ENCHANTS.add(ModEnchantments.COMET_STRIKE.get());
-            BOOT_ENCHANTS.add(ModEnchantments.DOUBLE_JUMP.get());
-            BOOT_ENCHANTS.add(ModEnchantments.MOMENTUM.get());
-            BOOT_ENCHANTS.add(ModEnchantments.SKY_CHARGE.get());
-            BOOT_ENCHANTS.add(ModEnchantments.STONE_CALLER.get());
-        }
-        if (WEAPON_ENCHANTS.isEmpty()) {
-            WEAPON_ENCHANTS.add(ModEnchantments.BLITZ.get());
-            WEAPON_ENCHANTS.add(ModEnchantments.BREAKTHROUGH.get());
-            WEAPON_ENCHANTS.add(ModEnchantments.BACKPEDAL.get());
-            WEAPON_ENCHANTS.add(ModEnchantments.GUST.get());
-            WEAPON_ENCHANTS.add(ModEnchantments.KINETIC.get());
-            WEAPON_ENCHANTS.add(ModEnchantments.PHANTOM_PAIN.get());
-            WEAPON_ENCHANTS.add(ModEnchantments.SKEWERING.get());
-            WEAPON_ENCHANTS.add(ModEnchantments.TEMPO_THEFT.get());
-        }
-        if (BOW_ENCHANTS.isEmpty()) {
-            BOW_ENCHANTS.add(ModEnchantments.ACCELERATE.get());
-            BOW_ENCHANTS.add(ModEnchantments.PHASING.get());
-            BOW_ENCHANTS.add(ModEnchantments.WHISTLER.get());
-        }
-        if (CROSSBOW_ENCHANTS.isEmpty()) {
-            CROSSBOW_ENCHANTS.add(ModEnchantments.MITOSIS.get());
-            CROSSBOW_ENCHANTS.add(ModEnchantments.REND.get());
-            CROSSBOW_ENCHANTS.add(ModEnchantments.SCATTER.get());
-        }
-        if (TRIDENT_ENCHANTS.isEmpty()) {
-            TRIDENT_ENCHANTS.add(ModEnchantments.EXTRACT.get());
-            TRIDENT_ENCHANTS.add(ModEnchantments.RECOIL.get());
-            TRIDENT_ENCHANTS.add(Enchantments.RIPTIDE);
-        }
-        if (TOOL_ENCHANTS.isEmpty()) {
-            TOOL_ENCHANTS.add(Enchantments.SILK_TOUCH);
-            TOOL_ENCHANTS.add(ModEnchantments.BRUTE_TOUCH.get());
-        }
-    }
+
+
 }
